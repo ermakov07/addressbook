9f176a768955224d84ac7f95276c5d1aa752ac8e
ermakov07 <ermakov07@mail.ru>
2019-10-10 15:46:13 +0300

    formation of the main page

--- people/views.py
+++ people/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 from .models import ListPeople
+from .models import Category
 
 
 def index(request):
     lp = ListPeople.objects.all()
-    return render(request, 'people/index.html', {'lp': lp})
+    categories = Category.objects.all()
+    context = {'lp': lp, 'categories': categories}
+    return render(request, 'people/index.html', context)
+
+def by_category(request, category_id):
+    lp = ListPeople.objects.filter(category=category_id)
+    categories = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+    context = {'lp': lp, 'categories': categories, 'current_category': current_category}
+    return render(request, 'people/by_category.html', context)
+
