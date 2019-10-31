a817d606da7a17f60bc46aed8aaec8d909371cc8
ermakov07 <ermakov07@mail.ru>
2019-10-31 13:58:43 +0300

    added widget to birthday box and category name value validation

--- people/forms.py
+++ people/forms.py
@@ -1,4 +1,8 @@
+import datetime
 from django.forms import ModelForm
+from django.forms import widgets
+from django.core.exceptions import ValidationError
+import re
 
 from .models import ListPeople
 from .models import Category
@@ -9,6 +13,8 @@ class PeopleForm(ModelForm):
     class Meta(object):
         model = ListPeople
         fields = ('fam', 'name', 'ot', 'addr', 'birthday', 'note', 'category')
+        year = datetime.date.today().year
+        widgets = {'birthday': widgets.SelectDateWidget(years=range(year, year-200, -1))}
 
 
 class CategoryForm(ModelForm):
@@ -16,3 +22,10 @@ class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
+    def clean(self):
+        errors = {}
+        if re.search(r'\W', self.cleaned_data['name']) :
+            errors['name'] = 'Ошибка, в названии категории должны быть буквы или цифры'
+        if errors:
+            raise ValidationError(errors)
